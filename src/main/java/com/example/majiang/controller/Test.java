ce94f181d3a50b75af5d7ac8b65acf25112a219c
fbMac <1713560766@qq.com>
2019-08-16 11:19:45 +0800

    first commit

---
+++ src/main/java/com/example/majiang/controller/Test.java
@@ -0,0 +1,18 @@
+package com.example.majiang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/test/")
+public class Test {
+
+    @GetMapping("/index")
+    public String index(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "test";
+    }
+}
